a3513eba5d93063dc13b40ab925cb2921eef4971
Abdullah-A-Elsayed <abdullah.ali26@hotmail.com>
2018-02-13 23:43:23 +0200

    drawing circle of squares is done

--- using_turtle.py
+++ using_turtle.py
@@ -1,22 +1,21 @@
 import turtle
-
+window = turtle.Screen()
+window.bgcolor("#111111")
+abdo = turtle.Turtle()
 def draw_square():
-	window = turtle.Screen()
-	window.bgcolor("#111111")
-	abdo = turtle.Turtle()
 	abdo.shape("turtle")
 	abdo.color("#FFFFFF")
-	abdo.speed(1)
+	abdo.speed(6)
 	abdo.pensize(3)
 	abdo.shapesize(4)
-	abdo.forward(150)
-	abdo.right(90)
-	abdo.forward(150)
-	abdo.right(90)
-	abdo.forward(150)
-	abdo.right(90)
-	abdo.forward(150)
-	abdo.right(90)
-	window.exitonclick()
-
-draw_square()
+	i=0
+	while (i<4):
+		abdo.forward(150)
+		abdo.right(90)
+		i+=1
+i =0
+while i<36:
+	draw_square()
+	abdo.right(10)
+	i+=1
+window.exitonclick()
